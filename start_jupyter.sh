c24a89170325f2efa4520f51ffcd8224b8d4be70
Cloud User <centos@ip-10-120-6-208.us-west-2.compute.internal>
2021-09-24 03:49:34 +0000

    Changed to docker container, moved config file outside container

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,1 +1,3 @@
-nohup jupyter lab &
+docker run -p 8888:8888 -v work:/home/jovyan/work \
+  -v keys:/keys -v jupyter_notebook_config.py:/home/jovyan/.jupyter/jupyter_notebook_config.py \
+  jupyter/scipy-notebook:latest
